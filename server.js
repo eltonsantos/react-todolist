d039002164796d25b665ec634e53e4c5c7f0def5
Elton Santos <eltonaxl@hotmail.com>
2022-01-20 01:55:12 -0300

    Configuring mongodb

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.json());
@@ -18,7 +18,12 @@ app.post("/name", (req, res) => {
   }
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("Connect to database");
+  
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
+}).catch((error) => {
+  console.error(error);
 });
-
