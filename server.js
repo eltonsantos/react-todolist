dd1b1ce3c3803d4e37cb1330fa875ded1586dda4
Elton Santos <eltonaxl@hotmail.com>
2022-01-23 01:21:16 -0300

    Preparing to deploy

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
+// import routes
 const authRoute = require("./routes/auth");
 const toDosRoute = require("./routes/todos");
 
@@ -13,18 +15,27 @@ app.use(express.urlencoded());
 app.use(cookieParser());
 
 app.get("/api", (req, res) => {
-  res.send("Todo list server working!")
+  res.send("Fullstack React Course Express Server");
 });
 
 app.use("/api/auth", authRoute);
 app.use("/api/todos", toDosRoute);
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  console.log("Connect to database");
-  
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-  });
-}).catch((error) => {
-  console.error(error);
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to database");
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
